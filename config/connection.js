ac25dc5032433e055e9096a56852257ce88089e8
Shank <shank@Nomans-MacBook-Pro.local>
2019-03-16 14:00:26 -0500

    lastpush

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,16 @@
 // Set up MySQL connection.
 var mysql = require('mysql');
-
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'lockdee23',
+		database: 'burger_db'
+	});
+}
 var connection = mysql.createConnection({
 	port: 3306,
 	host: 'localhost',
